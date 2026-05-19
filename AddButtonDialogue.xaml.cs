5c19b79ec81f61feb4fe3d9649809b0f4b22859c
Ifrit <Ifrit528@gmail.com>
2026-05-19 15:40:56 -0600

    Initial commit

---
+++ AddButtonDialogue.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Backup_Manager
+{
+    /// <summary>
+    /// Interaction logic for AddButtonDialogue.xaml
+    /// </summary>
+    public partial class AddButtonDialogue : Window
+    {
+        string sourcePath;
+        string destinationPath;
+
+        public AddButtonDialogue()
+        {
+            InitializeComponent();
+        }
+
+        public void SourceFolderDialogClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFolderDialog dialog = new();
+            dialog.Multiselect = false;
+            dialog.Title = "Select a Source Folder";
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                this.SourceResponseBox.Text = dialog.FolderName;
+            }
+        }
+        
+        public void DestinationFolderDialogClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFolderDialog dialog = new();
+            dialog.Multiselect = false;
+            dialog.Title = "Select a Destination Folder";
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                this.DestinationResponseBox.Text = dialog.FolderName;
+            }
+        }
+
+        public async void SubmitClick(object sender, RoutedEventArgs e)
+        {
+            sourcePath = this.SourceResponseBox.Text;
+            destinationPath = this.DestinationResponseBox.Text;
+
+            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            MainWindow.SaveObject saveObject = new MainWindow.SaveObject(sourcePath, destinationPath);
+            mainWindow.PathList.Add(saveObject);
+
+            await MainWindow.Serializer.SaveToFile();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Backup_Manager
+{
+    public partial class MainWindow : Window
+    {
+        static public readonly MainWindow currentWindow = (MainWindow)Application.Current.MainWindow;
+        static public readonly string fileName = "paths.json";
+
+        public MainWindow()
+        {
+            pathList = [];
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Serializer.ReadSaveFile();
+        }
+
+        private ObservableCollection<SaveObject> pathList;
+        public ObservableCollection<SaveObject> PathList
+        {
+            get { return pathList; }
+            set { pathList = value; }
+        }
+
+        public class SaveObject
+        {
+            public SaveObject(string source, string destination)
+            {
+                Source = source;
+                Destination = destination;
+            }
+
+            public string Source { get; set; }
+            public string Destination { get; set; }
+
+            public string GetTime
+            {
+                get
+                {
+                    return System.DateTime.Now.ToString("d");
+                }
+            }
+        }
+
+        public static class Serializer
+        {
+            public static async Task SaveToFile()
+            {
+                ObservableCollection<SaveObject> list = currentWindow.PathList;
+                await using FileStream fileStream = File.Create(fileName);
+                await JsonSerializer.SerializeAsync(fileStream, list);
+            }
+
+            public static async Task ReadSaveFile()
+            {
+                if (File.Exists(fileName))
+                {
+                    using FileStream openStream = File.OpenRead(fileName);
+                    ObservableCollection<SaveObject> saveObject = await JsonSerializer.DeserializeAsync<ObservableCollection<SaveObject>>(openStream);
+                    foreach (SaveObject obj in saveObject)
+                    {
+                        currentWindow.pathList.Add(obj);
+                    }
+                }
+            }
+        }
+
+        private void List_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            SaveObject saveObject = (SaveObject)button.DataContext;
+            MessageBox.Show($"{saveObject.Source}, {saveObject.Destination}");
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Window selectionWindow = new AddButtonDialogue();
+            selectionWindow.Show();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            ListView listView = this.ListView1;
+            SaveObject selectedItem = (SaveObject)listView.SelectedItem;
+
+            if (listView.SelectedItem != null)
+            {
+                MessageBox.Show(selectedItem.Source);
+            } else
+            {
+                MessageBox.Show("Nothing is selected.");
+            }
+        }
+
+    }
+}
